297f2d6d6894a6f90cd7649b2411cff6af2c6c73
fo-ji <fumiya1753@gmail.com>
2023-07-26 12:00:30 +0900

    implement Header, Footer component

--- client/app/(public)/layout.tsx
+++ client/app/(public)/layout.tsx
@@ -1,32 +1,7 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import type { ReactNode } from 'react';
 
-// TODO: component&Storybook化
-const Header = () => {
-  return (
-    <header className="flex h-14 items-center px-4 py-2">
-      <Link href="/" className="inline-block">
-        <Image
-          src="/logo.png"
-          alt="Anti-Patterns"
-          width={120}
-          height={34}
-          priority
-        />
-      </Link>
-    </header>
-  );
-};
-
-// TODO: component&Storybook化
-const Footer = () => {
-  return (
-    <footer className="flex h-14 items-center justify-center bg-primary px-4 py-2 text-secondary">
-      [仮置]footer
-    </footer>
-  );
-};
+import { Footer } from '@/components/footer';
+import { Header } from '@/components/header';
 
 export default function PublicLayout({ children }: { children: ReactNode }) {
   return (
--- client/app/(public)/page.tsx
+++ client/app/(public)/page.tsx
@@ -1,9 +1,5 @@
-import { Link } from '@/components/link';
-
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-between gap-12 p-24">
-      <Link href="/login">ログイン</Link>
-    </div>
+    <div className="flex flex-col items-center justify-between gap-12 p-24"></div>
   );
 }
