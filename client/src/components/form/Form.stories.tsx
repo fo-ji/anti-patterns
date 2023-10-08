d9c6693a7d36f25e9473d0ae78f02a71ec695761
fo-ji <fumiya1753@gmail.com>
2023-10-08 12:20:55 +0900

    add form components

--- client/src/components/form/Form.stories.tsx
+++ client/src/components/form/Form.stories.tsx
@@ -1,12 +1,22 @@
 import { Button } from '../button';
 
-import { Form } from './Form';
-import { InputField } from './InputField';
+import { CheckboxField } from './checkbox-field';
+import { CheckboxGroupField } from './checkbox-group-field';
+import { Form } from './form';
+import { InputField } from './input-field';
+import { RadioGroupField } from './radio-group-field';
+import { SelectField } from './select-field';
 
 import type { Meta, StoryFn } from '@storybook/react';
 
 type FormValues = {
   title: string;
+  email: string;
+  password: string;
+  isChecked: boolean;
+  radioGroups: string[];
+  selects: string[];
+  checkboxGroups: string[];
 };
 
 const MyForm = () => {
@@ -18,11 +28,63 @@ const MyForm = () => {
       id="my-form"
     >
       {({ register, formState }) => (
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-8 rounded bg-white p-8">
           <InputField
             label="Title"
+            placeholder="Let's type!"
             error={formState.errors['title']}
             registration={register('title')}
+          />
+          <InputField
+            type="email"
+            label="Email"
+            placeholder="user@example.com"
+            error={formState.errors['email']}
+            registration={register('email')}
+          />
+          <InputField
+            type="password"
+            label="Password"
+            placeholder="Test12345!"
+            error={formState.errors.password}
+            registration={register('password')}
+          />
+          <CheckboxField
+            label="Checkbox"
+            error={formState.errors.isChecked}
+            registration={register('isChecked')}
+          />
+          <RadioGroupField
+            legend="RadioGroup"
+            error={formState.errors.radioGroups}
+            options={[
+              { label: 'radio1', value: '1' },
+              { label: 'radio2', value: '2' },
+              { label: 'radio3', value: '3' },
+            ]}
+            registration={register('radioGroups')}
+          />
+          <SelectField
+            label="Select"
+            error={formState.errors.selects}
+            options={[
+              { label: 'select1', value: '1' },
+              { label: 'select2', value: '2' },
+              { label: 'select3', value: '3' },
+              { label: 'select4', value: '4' },
+              { label: 'select5', value: '5' },
+            ]}
+            registration={register('selects')}
+          />
+          <CheckboxGroupField
+            legend="CheckboxGroup"
+            error={formState.errors.checkboxGroups}
+            options={[
+              { label: 'checkbox1', value: '1' },
+              { label: 'checkbox2', value: '2' },
+              { label: 'checkbox3', value: '3' },
+            ]}
+            registration={register('checkboxGroups')}
           />
           <div>
             <Button type="submit">Submit</Button>
